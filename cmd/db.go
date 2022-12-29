53e0a8ae0e545e9492618e6d9b5e45f77e43e6d3
Michael Ericson <maerics@gmail.com>
2022-12-29 15:46:20 -0700

    Add "db {execute,select}" cmds

--- cmd/db.go
+++ cmd/db.go
@@ -1,6 +1,14 @@
 package cmd
 
 import (
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
 	"webapp/db"
 
 	log "github.com/maerics/golog"
@@ -13,13 +21,160 @@ import (
 func init() {
 	rootCmd.AddCommand(dbCmd)
 
+	dbCmd.AddCommand(selectCmd)
+	dbCmd.AddCommand(executeCmd)
 	dbCmd.AddCommand(migrateCmd)
+
+	executeCmd.Flags().BoolVarP(&optDbExecuteCommit,
+		"commit", "", false, "commit the transaction instead of rolling back")
+
+	selectCmd.Flags().BoolVarP(&optDbSelectCsvOutput,
+		"csv", "c", false, "format result set as CSV instead of JSON")
+	selectCmd.Flags().StringVarP(&optDbSelectCsvSep,
+		"sep", "s", ",", "separator to use for CSV output")
 }
+
+var (
+	optDbExecuteCommit   = false
+	optDbSelectCsvOutput = false
+	optDbSelectCsvSep    = ","
+)
 
 var dbCmd = &cobra.Command{
 	Use:   "db",
 	Short: "Manage the database.",
 	Run:   func(cmd *cobra.Command, args []string) { cmd.Help() },
+}
+
+var executeCmd = &cobra.Command{
+	Use:     "execute",
+	Aliases: []string{"exec", "e"},
+	Short:   "Execute SQL commands from STDIN.",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Read the query from STDIN.
+		buf := &bytes.Buffer{}
+		log.Must1(io.Copy(buf, os.Stdin))
+		query := strings.TrimSpace(buf.String())
+
+		// Execute the query.
+		dburl := util.MustEnv(Env_DATABASE_URL)
+		db := log.Must1(db.Connect(dburl))
+		tx := db.MustBegin()
+		log.Printf("executing query:\n\n  %v\n\n", query)
+		t0 := time.Now()
+
+		// Print the affected rows and last insert id, if applicable.
+		result := log.Must1(tx.Exec(query))
+		if n, err := result.RowsAffected(); err == nil {
+			log.Printf("query affected %v row(s)", n)
+		}
+		if n, err := result.LastInsertId(); err == nil {
+			log.Printf("last insert id=%v", n)
+		}
+
+		// Commit or rollback.
+		if optDbExecuteCommit {
+			log.Must(tx.Commit())
+			log.Printf("committed transaction in %v", time.Since(t0))
+		} else {
+			log.Must(tx.Rollback())
+			log.Printf("rolled back transaction in %v (see help for details)", time.Since(t0))
+		}
+	},
+}
+
+var selectCmd = &cobra.Command{
+	Use:     "select",
+	Aliases: []string{"sel", "s"},
+	Short:   "Print the results of a database query from STDIN to STDOUT.",
+	Run: func(cmd *cobra.Command, args []string) {
+		iscsv := optDbSelectCsvOutput
+		if iscsv {
+			if len(optDbSelectCsvSep) != 1 {
+				log.Fatalf("CSV separator must be one byte long, got %q", optDbSelectCsvSep)
+			}
+		}
+
+		// Read the query from STDIN.
+		buf := &bytes.Buffer{}
+		log.Must1(io.Copy(buf, os.Stdin))
+		query := strings.TrimSpace(buf.String())
+
+		// Execute the query.
+		dburl := util.MustEnv(Env_DATABASE_URL)
+		db := log.Must1(db.Connect(dburl))
+		log.Printf("executing query:\n\n  %v\n\n", query)
+		rows := log.Must1(db.Query(query))
+
+		// Inspect the result set column types.
+		columns := log.Must1(rows.Columns())
+		values := make([]any, len(columns))
+		scanArgs := make([]any, len(columns))
+		for i := range values {
+			scanArgs[i] = &values[i]
+		}
+
+		// Setup the optional CSV writer.
+		var w *csv.Writer
+		if iscsv {
+			w = csv.NewWriter(os.Stdout)
+			w.Comma = rune(optDbSelectCsvSep[0])
+			log.Must(w.Write(columns))
+		}
+
+		// Print to stdout.
+		count := 0
+		enc := json.NewEncoder(os.Stdout)
+		for rows.Next() {
+			count++
+			log.Must(rows.Scan(scanArgs...))
+
+			// Print the JSON or CSV result.
+			if iscsv {
+				row := make([]string, len(values))
+				for i, v := range values {
+					if v != nil {
+						row[i] = fmt.Sprintf("%v", v)
+					}
+				}
+				log.Must(w.Write(row))
+			} else {
+				/* TODO: retain column ordering
+				obj := map[string]any{}
+				for i, v := range values {
+					obj[columns[i]] = v
+				}
+				*/
+				log.Must(enc.Encode(orderedJsonObj{columns, values}))
+			}
+		}
+
+		if iscsv {
+			w.Flush()
+			log.Must(w.Error())
+		}
+		log.Printf("query returned %v row(s)", count)
+	},
+}
+
+type orderedJsonObj struct {
+	columns []string
+	values  []any
+}
+
+func (o orderedJsonObj) MarshalJSON() ([]byte, error) {
+	buf := &bytes.Buffer{}
+	buf.WriteByte('{')
+	for i, c := range o.columns {
+		if bs, err := json.Marshal(o.values[i]); err != nil {
+			return nil, err
+		} else {
+			fmt.Fprintf(buf, "%q:%v,", c, string(bs))
+		}
+	}
+	bs := buf.Bytes()
+	bs[len(bs)-1] = '}'
+	return bs, nil
 }
 
 var migrateCmd = &cobra.Command{
